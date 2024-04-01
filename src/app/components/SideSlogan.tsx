6080c7ebfb7d7275ce5cbfd2a29bc9605340e9c3
Curtis Huang <curtisahuang@gmail.com>
2024-04-01 22:42:06 +0100

    updating slogan

--- src/app/components/SideSlogan.tsx
+++ src/app/components/SideSlogan.tsx
@@ -25,10 +25,10 @@ const SideSlogan = () => {
       }}
     >
       <div ref={plane} className="absolute top-[20rem] right-[6rem] z-0">
-        <Text3D primary="creating" secondary="making" />
-        <Text3D primary="concepts" secondary="worlds" />
+        <Text3D primary="creating" secondary="delivering" />
+        <Text3D primary="concepts" secondary="products" />
         <Text3D primary="with" secondary="from" />
-        <Text3D primary="design" secondary="ux/ui" />
+        <Text3D primary="design" secondary="ui/ux" />
       </div>
     </div>
   );
