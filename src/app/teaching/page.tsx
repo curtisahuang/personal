7df853bc8f4b9756e77b1e253d40b4f7df376444
Curtis Huang <curtisahuang@gmail.com>
2026-08-07 16:13:21 +0100

    links

--- src/app/teaching/page.tsx
+++ src/app/teaching/page.tsx
@@ -5,6 +5,22 @@ import styles from "./teaching.module.css";
 
 const newsreader = Newsreader({ subsets: ["latin"] });
 
+function WhatsAppIcon() {
+  return (
+    <span aria-hidden="true">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        width="1em"
+        height="1em"
+      >
+        <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.098-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z" />
+      </svg>
+    </span>
+  );
+}
+
 export const metadata: Metadata = {
   title: "Teacher Curtis | Curtis Alexander Huang",
   description:
@@ -16,6 +32,9 @@ export default function TeachingPage() {
     <div className={`${styles.page} ${newsreader.className}`}>
       <main className={styles.main}>
         <header className={styles.masthead}>
+          <SwipeLink direction="left" href="/" className={styles.backLink}>
+            ← Back to homepage
+          </SwipeLink>
           <p className={styles.eyebrow}>Private educator</p>
           <h1 className={styles.title}>Teacher Curtis</h1>
           <div className={styles.contactList} aria-label="Contact information">
@@ -29,6 +48,29 @@ export default function TeachingPage() {
                 curtisahuang.com
               </SwipeLink>
             </p>
+            <p>
+              <span>Facebook</span>
+              <a
+                className={styles.socialIcon}
+                href="https://www.facebook.com/profile.php?id=61593049071956"
+                aria-label="Teacher Curtis on Facebook"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  width="1.15em"
+                  height="1.15em"
+                  aria-hidden="true"
+                >
+                  <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.413c0-3.026 1.792-4.698 4.533-4.698 1.312 0 2.686.236 2.686.236v2.971H15.83c-1.491 0-1.956.931-1.956 1.887v2.264h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z" />
+                </svg>
+              </a>
+            </p>
+            <a className={styles.topCta} href="https://wa.me/85255781337">
+              Start a conversation
+              <WhatsAppIcon />
+            </a>
           </div>
         </header>
 
@@ -194,12 +236,8 @@ export default function TeachingPage() {
         <footer className={styles.footer}>
           <p>Available for private tutoring and educational consultation.</p>
           <a href="https://wa.me/85255781337">
-            Start a conversation{" "}
-            <span aria-hidden="true">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" style={{ verticalAlign: "-0.15em", marginLeft: "0.25em" }}>
-                <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.098-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"/>
-              </svg>
-            </span>
+            Start a conversation
+            <WhatsAppIcon />
           </a>
         </footer>
       </main>
